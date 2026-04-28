1fd1465236abc304d09cc2708656d937f3713c2a
Khoa Truong <100bn@Khoas-MacBook-Air.local>
2026-04-29 00:48:30 +0700

    Add Get Url CDN

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,7 +5,21 @@ export const getFullStaticImageUrl = (path: string | null) => {
   if (path.startsWith('http')) return path;
 
   const baseUrl = (process.env?.NEXT_PUBLIC_ENV != 'development' ? process.env.NEXT_PUBLIC_STATIC_SUPABASE_URL : process.env.NEXT_PUBLIC_STATIC_LOCAL_URL);
-  
+
+  // Đảm bảo không bị dư dấu / giữa baseUrl và path
+  const cleanPath = path.startsWith('/') ? path : `/${path}`;
+
+  return `${baseUrl}${cleanPath}`;
+};
+
+export const getFullCDNUrl = (path: string | null) => {
+  if (!path) return ;
+
+  // Nếu path đã là link tuyệt đối (http...) thì trả về luôn
+  if (path.startsWith('http')) return path;
+
+  const baseUrl = (process.env?.NEXT_PUBLIC_ENV != 'development' ? process.env.NEXT_PUBLIC_CDN_URL : process.env.NEXT_PUBLIC_CDN_LOCAL_URL);
+
   // Đảm bảo không bị dư dấu / giữa baseUrl và path
   const cleanPath = path.startsWith('/') ? path : `/${path}`;
 
